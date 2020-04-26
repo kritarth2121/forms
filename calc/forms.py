34b30d15a518831a5d02d8ccc96d8c8c56064c44
kritarth2121 <44065192+kritarth2121@users.noreply.github.com>
2020-04-26 11:04:20 +0530

    Update forms.py

--- calc/forms.py
+++ calc/forms.py
@@ -1,37 +1,37 @@
-from .models import Review
+from .models import BasicChecku,ElectricalChecku,MechanicalChecku,InstallationChecku,ChilledwaterChecku,Name
 from django import forms
 from django.forms import widgets
 
 class BasicCheckup(forms.ModelForm):
     class Meta:
-        model=Review
-        fields=['BasicChecku']
-        widgets = { 'BasicChecku': forms.TextInput(attrs={'size': 200})}
+        model=BasicChecku
+        fields=['description']
+        widgets = { 'description': forms.TextInput(attrs={'size': 200})}
 class ElectricalCheckup(forms.ModelForm):
     class Meta:
-        model=Review
-        fields=['ElectricalChecku']
-        widgets = { 'ElectricalChecku': forms.TextInput(attrs={'size': 200})}
+        model=ElectricalChecku
+        fields=['description']
+        widgets = { 'description': forms.TextInput(attrs={'size': 200})}
 
 class MechanicalCheckup(forms.ModelForm):
     class Meta:
-        model=Review
-        fields=['MechanicalChecku']
-        widgets = { 'MechanicalChecku': forms.TextInput(attrs={'size': 200})}
+        model=MechanicalChecku
+        fields=['description']
+        widgets = { 'description': forms.TextInput(attrs={'size': 200})}
 
 class InstallationCheckup(forms.ModelForm):
     class Meta:
-        model=Review
-        fields=['InstallationChecku']
-        widgets = { 'InstallationChecku': forms.TextInput(attrs={'size': 200})}
+        model=InstallationChecku
+        fields=['description']
+        widgets = { 'description': forms.TextInput(attrs={'size': 200})}
 
 class ChilledwaterCheckup(forms.ModelForm):
     class Meta:
-        model=Review
-        fields=['ChilledwaterChecku']
-        widgets = { 'ChilledwaterChecku': forms.TextInput(attrs={'size': 200})}
+        model=ChilledwaterChecku
+        fields=['description']
+        widgets = { 'description': forms.TextInput(attrs={'size': 200})}
 class Namee(forms.ModelForm):
     class Meta:
-        model=Review
-        fields=['Name']
-        widgets = { 'Name': forms.TextInput(attrs={'size': 200})}
+        model=Name
+        fields=['description']
+        widgets = { 'description': forms.TextInput(attrs={'size': 200})}
