5ad6fa209c3206016ac24f4021ee46db89bb93c1
kritarth2121 <44065192+kritarth2121@users.noreply.github.com>
2020-04-26 11:04:52 +0530

    Update admin.py

--- calc/admin.py
+++ calc/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Review
+from .models import BasicChecku,ElectricalChecku,MechanicalChecku,InstallationChecku,ChilledwaterChecku,Name
 # Register your models here.
-admin.site.register(Review)
+admin.site.register(Name)
 
+admin.site.register(BasicChecku)
+admin.site.register(ElectricalChecku)
+admin.site.register(MechanicalChecku)
+admin.site.register(InstallationChecku)
+admin.site.register(ChilledwaterChecku)
